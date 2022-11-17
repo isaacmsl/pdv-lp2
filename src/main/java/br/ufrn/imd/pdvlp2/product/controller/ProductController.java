1ee5f97ff37c7e96b154fa266699a3959dbd7520
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-11-16 23:31:17 -0300

    feat: Product @Service @Controller and remove patch

--- src/main/java/br/ufrn/imd/pdvlp2/product/controller/ProductController.java
+++ src/main/java/br/ufrn/imd/pdvlp2/product/controller/ProductController.java
@@ -1,9 +1,14 @@
 package br.ufrn.imd.pdvlp2.product.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.ufrn.imd.pdvlp2.core.controller.AbstractController;
 import br.ufrn.imd.pdvlp2.product.model.ProductModel;
 import br.ufrn.imd.pdvlp2.product.service.ProductService;
 
+@RestController
+@RequestMapping("products")
 public class ProductController extends AbstractController<ProductModel, ProductService>{
     
 }
--- src/main/java/br/ufrn/imd/pdvlp2/product/service/ProductService.java
+++ src/main/java/br/ufrn/imd/pdvlp2/product/service/ProductService.java
@@ -1,15 +1,13 @@
 package br.ufrn.imd.pdvlp2.product.service;
 
-import br.ufrn.imd.pdvlp2.core.model.AbstractModel;
+import java.util.Objects;
+
+import org.springframework.stereotype.Service;
+
 import br.ufrn.imd.pdvlp2.core.service.AbstractService;
 import br.ufrn.imd.pdvlp2.product.model.ProductModel;
 import br.ufrn.imd.pdvlp2.product.repository.ProductRepository;
 
+@Service
 public class ProductService extends AbstractService<ProductModel, ProductRepository> {
-
-    @Override
-    public ProductModel patch(ProductModel model, ProductModel saveModel) {
-        // TODO Auto-generated method stub
-        return null;
-    }
 }
