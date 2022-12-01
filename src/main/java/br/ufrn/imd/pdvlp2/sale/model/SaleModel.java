7271d6d93497a8157bf05b0fcc0a433515685f5f
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-12-01 15:46:07 -0300

    feat: Sale entity

---
+++ src/main/java/br/ufrn/imd/pdvlp2/sale/model/SaleModel.java
@@ -0,0 +1,41 @@
+package br.ufrn.imd.pdvlp2.sale.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import br.ufrn.imd.pdvlp2.core.model.AbstractModel;
+import br.ufrn.imd.pdvlp2.paymentWay.model.PaymentWayModel;
+import br.ufrn.imd.pdvlp2.product.model.ProductModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor; 
+
+@Document("Sale")
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=false)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleModel extends AbstractModel {
+
+    private PaymentWayModel paymentWay;
+    
+    @OneToMany
+    @JoinColumn(name="sale_id")
+    List<ProductModel> products;
+
+    public double getTotal() {
+        double totalProducts = products.stream().reduce(0.0, (subTotal, element) -> subTotal + element.getPrice(), Double::sum);
+        return totalProducts * (1 + paymentWay.getTax());
+    }
+
+
+}
