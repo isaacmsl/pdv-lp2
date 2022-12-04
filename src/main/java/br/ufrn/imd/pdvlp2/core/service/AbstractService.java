ed993158b77328513ea1a9b4049305567e223f70
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-12-04 18:09:15 -0300

    feat: log when save delete AbstractMOdel

--- src/main/java/br/ufrn/imd/pdvlp2/core/service/AbstractService.java
+++ src/main/java/br/ufrn/imd/pdvlp2/core/service/AbstractService.java
@@ -2,15 +2,21 @@ package br.ufrn.imd.pdvlp2.core.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import br.ufrn.imd.pdvlp2.core.model.AbstractModel;
 import br.ufrn.imd.pdvlp2.core.repository.AbstractRepository;
+import br.ufrn.imd.pdvlp2.log.model.LogModel;
+import br.ufrn.imd.pdvlp2.log.repository.LogRepository;
 
 public abstract class AbstractService <M extends AbstractModel, R extends AbstractRepository<M>> {
     @Autowired
     private R repository;
+
+    @Autowired
+    private LogRepository log;
 
     public List<M> findAll() {
         return repository.findAll();
@@ -22,11 +28,13 @@ public abstract class AbstractService <M extends AbstractModel, R extends Abstra
 
     public synchronized M save(M model) throws Exception {
         M savedModel = repository.save(model);
+        log.save(new LogModel("System", "has saved/updated a " + model.getClass().getSimpleName(), model.getId(), null, LocalDateTime.now()));
         return savedModel;
     }
 
     public void delete(M model) {
         repository.delete(model);
+        log.save(new LogModel("System", "has deleted a " + model.getClass().getSimpleName(), model.getId(), null, LocalDateTime.now()));
     }
 
 }
